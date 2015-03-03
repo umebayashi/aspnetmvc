63e051ba83dfc0b13ea65f9a97adda8ca72ec7e0
Akihiro Takada <umebayashi.akihiro@gmail.com>
2015-03-03 13:54:15 +0900

    Memberクラス追加

---
+++ Training/MvcBasic/MvcBasic/Models/Member.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MvcBasic.Models
+{
+	public class Member
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Email { get; set; }
+
+		public DateTime Birth { get; set; }
+
+		public bool Married { get; set; }
+
+		public string Memo { get; set; }
+	}
+}
