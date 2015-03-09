fb36eaef17d37b6757309bb82f069ff9370b22d0
Akihiro Takada <umebayashi.akihiro@gmail.com>
2015-03-09 17:14:12 +0900

    Chapter3まで完了

--- Training/MvcBasic/MvcBasic/Models/Member.cs
+++ Training/MvcBasic/MvcBasic/Models/Member.cs
@@ -1,19 +1,24 @@
 ﻿using System;
-
+using System.ComponentModel;
 namespace MvcBasic.Models
 {
 	public class Member
 	{
 		public int Id { get; set; }
 
+		[DisplayName("氏名")]
 		public string Name { get; set; }
 
+		[DisplayName("メールアドレス")]
 		public string Email { get; set; }
 
+		[DisplayName("生年月日")]
 		public DateTime Birth { get; set; }
 
+		[DisplayName("既婚")]
 		public bool Married { get; set; }
 
+		[DisplayName("自己紹介")]
 		public string Memo { get; set; }
 	}
 }
